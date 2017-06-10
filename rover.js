c3dc1d9d009466586af9ca37f24fbc571a48bc02
Carmen Fuentes <carfuentesrosales@gmail.com>
2017-06-10 21:46:16 +0200

    Just the comments and readme left

--- rover.js
+++ rover.js
@@ -3,13 +3,18 @@ var myRover = {
   direction: 'N'
 };
 
+var myEve = {
+  position: [0,0],
+  direction: 'N'
+};
+
 var obstacles = [
 [4,5],
 [6,2],
 [9,7],
 ];
 
-function gridGenerator () {
+function gridGenerator (obstacles) {
   var myGrid=[[]];
     for (var i=0; i<10; i++) {
       myGrid[i]=[0];
@@ -17,6 +22,11 @@ function gridGenerator () {
         myGrid[i][j]=0;
       }
     }
+
+    for (i=0; i<=obstacles.length-1; i++) {
+      myGrid[obstacles[i][0]][obstacles[i][1]]="*";
+    }
+
     return myGrid;
 }
 
@@ -118,15 +128,12 @@ function stopObstacle (rover, obstacles) {
   }
 }
 
-function posGrid (rover,obstacles) {
-myGrid[rover.position[0]][rover.position[1]]=1;
-  for (var i=0; i<=obstacles.length-1; i++) {
-    myGrid[obstacles[i][0]][obstacles[i][1]]="*";
-    }
+function posGrid (rover,flag) {
+  myGrid[rover.position[0]][rover.position[1]]=flag;
   return myGrid;
 }
 
-function moveRover (rover,obstacles){
+function moveRover (rover,obstacles,flag){
   stopObstacle(rover,obstacles);
   for (var i=0; i<=rover.position.length-1; i++) {
     if (rover.position[i] > 10) {
@@ -136,14 +143,28 @@ function moveRover (rover,obstacles){
       rover.position[i] = 10 + rover.position[i];
     }
   }
-    myGrid= gridGenerator();
-    myGrid= posGrid(rover, obstacles);
+    myGrid= posGrid(rover, flag);
+    return myGrid;
 
 }
 
+function avoidTheMate (rover,eve,flag) {
+  if (rover.position.toString() == eve.position.toString()) {
+    rover.position[0]=rover.position[0]-1;
+      console.log("Ups! Sorry mate, I'll move");
+  }
+  stopObstacle(rover,obstacles);
+  myGrid= posGrid(rover, flag);
+}
 
-/*moveRover("fffffff",myRover);*/
-myRover.position=[4,5];
-moveRover(myRover, obstacles);
+myGrid= gridGenerator(obstacles);
+doInstructions("ffffffffrffff", myRover);
+moveRover(myRover, obstacles,"r");
+doInstructions("ffffffffrffff", myEve);
+moveRover(myEve, obstacles,"e");
+avoidTheMate(myRover,myEve,"r");
+
+
 console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]");
+console.log("New Rover Position: [" + myEve.position[0] + ", " + myEve.position[1] + "]");
 console.log(myGrid);
