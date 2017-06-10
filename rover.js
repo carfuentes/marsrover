549c77b3fbf2612cf07a7be2ec0e6da14c4a647c
Carmen Fuentes <carfuentesrosales@gmail.com>
2017-06-10 20:47:30 +0200

    Almostish finished

--- rover.js
+++ rover.js
@@ -3,6 +3,24 @@ var myRover = {
   direction: 'N'
 };
 
+var obstacles = [
+[4,5],
+[6,2],
+[9,7],
+];
+
+function gridGenerator () {
+  var myGrid=[[]];
+    for (var i=0; i<10; i++) {
+      myGrid[i]=[0];
+      for (var j=0; j<10; j++) {
+        myGrid[i][j]=0;
+      }
+    }
+    return myGrid;
+}
+
+
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
@@ -73,7 +91,7 @@ function turnLeft(rover) {
 
 }
 
-function moveRover (instructions,rover) {
+function doInstructions (instructions,rover) {
   for (var i=0; i<=instructions.length-1; i++) {
     if (instructions[i] =="f") {
       goForward(rover);
@@ -90,15 +108,42 @@ function moveRover (instructions,rover) {
   return rover;
 }
 
-function posGrid (rover){
+
+function stopObstacle (rover, obstacles) {
+  for (var i in obstacles) {
+    if (rover.position.toString() == obstacles[i].toString()) {
+      rover.position[0]=rover.position[0]-1;
+      console.log("Found an obstacle in position: "+obstacles[i]);
+    }
+  }
+}
+
+function posGrid (rover,obstacles) {
+myGrid[rover.position[0]][rover.position[1]]=1;
+  for (var i=0; i<=obstacles.length-1; i++) {
+    myGrid[obstacles[i][0]][obstacles[i][1]]="*";
+    }
+  return myGrid;
+}
+
+function moveRover (rover,obstacles){
+  stopObstacle(rover,obstacles);
   for (var i=0; i<=rover.position.length-1; i++) {
     if (rover.position[i] > 10) {
       rover.position[i] = rover.position[i] % 10;
     }
-  return rover;
-  }}
+    else if (rover.position[i] < 0) {
+      rover.position[i] = 10 + rover.position[i];
+    }
+  }
+    myGrid= gridGenerator();
+    myGrid= posGrid(rover, obstacles);
 
-moveRover("fffffffffff",myRover);
+}
+
+
+/*moveRover("fffffff",myRover);*/
+myRover.position=[4,5];
+moveRover(myRover, obstacles);
 console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]");
-posGrid(myRover);
-console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]");
+console.log(myGrid);
